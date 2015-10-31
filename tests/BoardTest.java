ddafe4422bf7748394aca1e0fb5361e20fdbccda
Collin L. Walling <collin.walling@gmail.com>
2015-10-31 17:25:11 -0400

    Move validation check created and passes.

--- tests/BoardTest.java
+++ tests/BoardTest.java
@@ -4,10 +4,11 @@ import org.junit.*;
 public class BoardTest {
 
     Board b;
-
+    int xMax = 10;
+    int yMax = 10;
     @Before
     public void createBoard() {
-        b = new Board(5,5,5,5,5);
+        b = new Board(xMax,yMax,5,5,5);
     }
 
     @Test
@@ -15,6 +16,13 @@ public class BoardTest {
         for (int i = 0; i < 5; i++)
             for (int j = 0; j < 5; j++)
                 System.out.println(b.getNode(i,j));
+    }
+
+    @Test
+    public void testValidMoves() {
+        for (int i = 0; i < xMax; i++)
+            for (int j = 0; j < yMax; j++)
+                assertTrue(b.isValidMove(i,j));
     }
 
     @After
