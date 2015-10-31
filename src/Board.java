e54899ae7a9cdb172b5d6487447aeb7876f75969
Collin L. Walling <collin.walling@gmail.com>
2015-10-31 11:31:02 -0400

    Added bare Board class.
    This will be the object that contains all the nodes and what
    the player will traverse on

---
+++ src/Board.java
@@ -0,0 +1,13 @@
+/** Board.java
+  * ==========
+  * Main movement grid of the game world.
+  */
+
+class Board {
+
+    public static void main(String[] args) {
+        System.out.println("Warrior sword");
+    }    
+
+
+}
