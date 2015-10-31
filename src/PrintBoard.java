85f8234983eefa148bc38259965526065019a597
Collin L. Walling <collin.walling@gmail.com>
2015-10-31 16:54:24 -0400

    PrintBoard class to display text based board.

---
+++ src/PrintBoard.java
@@ -0,0 +1,15 @@
+public class PrintBoard {
+
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 5;
+
+        Board b = new Board(x,y,5,5,5);
+
+        for (int i = 0; i < x; i++)
+            for (int j = 0; j < y; j++)
+                System.out.println(b.getNode(i,j));
+
+    }
+
+}
