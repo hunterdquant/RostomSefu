d52399d66349a29ad1bd684f03ba4f048b94d51c
Collin L. Walling <collin.walling@gmail.com>
2015-10-31 16:54:43 -0400

    Tests: template and unnecessary board test

---
+++ tests/TestTemplate.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class TestTemplate {
+
+    @Before
+    public void doBeforeEveryTest() {
+
+    }
+
+    @Test
+    public void actualyTest() {
+
+    }
+
+    @After
+    public void doAfterEveryTest() {
+
+    }
+
+}
